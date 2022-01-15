d5f258c09896c768cce1a5e0b5c3591f8c5f58bb
Yaw Asamoah <yawasamoah13@hotmail.com>
2022-01-15 15:37:27 -0500

    update Script

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 
-var gifPics = ["bg4.gif","bg4.gif","bg4.gif","bg4.gif"];
-var normPics = ["orig.jpg","orig.jpg","orig.jpg","orig.jpg"];
-
-function gifImg(pic_id)
-{
-    var x = document.getElementById(pic_id);
-    x.src = gifPics[pic_id];
-    //x.style.opacity = "1"
-}
-
-function normalImg(pic_id)
-{
-    var x = document.getElementById(pic_id);
-    x.src = normPics[pic_id];
-    //x.style.opacity = "0.5"
-}
